dc4d39e6068e3c20b3d6103a6321af98e25e837c
bilgenm <bilgen.mustafa@condorcet.be>
2014-09-29 14:41:00 +0200

    Ajout liste

--- pages/liste_locaux.php
+++ pages/liste_locaux.php
@@ -1,2 +1,12 @@
-
+<h2>Liste Locaux</h2>
+<section>
+    <div>
+        <h3>Local F</h3>
+        <ul>
+            <li>Labo F3</li>
+            <li>Labo F4</li>
+            <li>Labo F5</li>  
+        </ul>
+    </div>  
+</section>
 
