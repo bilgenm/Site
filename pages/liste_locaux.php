a135ffb561cf601febc9f23f451fbd9dffba2b31
bilgenm <bilgen.mustafa@condorcet.be>
2014-10-05 22:54:42 +0200

    Commit Batiment E

--- pages/liste_locaux.php
+++ pages/liste_locaux.php
@@ -7,6 +7,12 @@
             <li>Labo F4</li>
             <li>Labo F5</li>  
         </ul>
+        <h3>Local E</h3>
+        <ul>
+            <li>Labo E3</li>
+            <li>Labo E4</li>
+            <li>Labo E5</li>  
+        </ul>
     </div>  
 </section>
 
